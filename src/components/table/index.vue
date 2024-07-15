9dcc3e4e624feb4384db318d616852bd710d792e
Ivan <xvantz@yandex.kz>
2024-07-15 13:12:24 +0700

    add new brands

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -2,23 +2,23 @@
   import style from './main.module.scss'
 
   const topBlocks = [
-    {name: "1", color: "red", type: "block", price: 60},
+    {name: "1", color: "red", type: "block", price: 60, class: "viber"},
     {name: "2", color: "none", type: "chance"},
-    {name: "3", color: "red", type: "block", price: 60},
+    {name: "3", color: "red", type: "block", price: 60, class: "whatsapp"},
     {name: "4", color: "none", type: "chance", price: 200},
-    {name: "5", color: "none", type: "chance", price: 200},
-    {name: "6", color: "yellow", type: "block", price: 100},
+    {name: "5", color: "none", type: "block", price: 200, class: "tesla"},
+    {name: "6", color: "yellow", type: "block", price: 100, class: "facebook"},
     {name: "7", color: "none", type: "chance"},
-    {name: "8", color: "yellow", type: "block", price: 100},
-    {name: "9", color: "yellow", type: "block", price: 120},
+    {name: "8", color: "yellow", type: "block", price: 100, class: "instagram"},
+    {name: "9", color: "yellow", type: "block", price: 120, class: "telegram"},
   ]
 
   const rightBlocks = [
     {name: "12", color: "green", price: 140, type: "block"},
-    {name: "13", color: "none", type: "chance"},
+    {name: "13", color: "none", price: 100, type: "block", class: "github"},
     {name: "14", color: "green", price: 140, type: "block"},
     {name: "15", color: "green", price: 160, type: "block"},
-    {name: "16", color: "none", type: "chance"},
+    {name: "16", color: "none", price: 200, type: "block", class: "rollsRoyce"},
     {name: "17", color: "blue", price: 180, type: "block"},
     {name: "18", color: "none", type: "chance"},
     {name: "19", color: "blue", price: 180, type: "block"},
@@ -30,10 +30,10 @@
     {name: "23", color: "none", type: "chance"},
     {name: "24", color: "brown", price: 220},
     {name: "25", color: "brown", price: 240},
-    {name: "26", color: "none", type: "chance"},
+    {name: "26", color: "none", price: 200, type: "block", class: "porsche"},
     {name: "27", color: "lightBlue", price: 260},
     {name: "28", color: "lightBlue", price: 260},
-    {name: "29", color: "none", type: "chance"},
+    {name: "29", color: "none", price: 100, type: "block", class: "adobe"},
     {name: "30", color: "lightBlue", price: 280},
   ].reverse()
 
@@ -42,11 +42,11 @@
     {name: "32", color: "none", type: "chance"},
     {name: "33", color: "purple", price: 300},
     {name: "34", color: "purple", price: 320},
-    {name: "35", color: "none", type: "chance"},
+    {name: "35", color: "none", price: 200, type: "block", class: "lamborghini"},
     {name: "36", color: "none", type: "chance"},
-    {name: "37", color: "orange", price: 350},
+    {name: "37", color: "orange", price: 350, type: "block", class: "huawei"},
     {name: "38", color: "none", type: "chance"},
-    {name: "39", color: "orange", price: 400},
+    {name: "39", color: "orange", price: 400, type: "block", class: "apple"},
   ].reverse()
 </script>
 
@@ -57,6 +57,7 @@
       <div :class="style.block" v-for="(block, index) in topBlocks" :key="index">
         <div :class="style[block.color]"/>
         <div :class="block.type === 'block' ? style.imageBlock : style.chanceBlock">
+          <div v-if="block.class" :class="style[block.class]"/>
           <div v-if="block.type === 'chance'" :class="style.chance"/>
         </div>
         <div v-if="block.price" :class="style.price">{{block.price}} M</div>
@@ -68,6 +69,7 @@
         <div :class="style.block" v-for="(block, index) in leftBlocks" :key="index">
           <div :class="style[block.color]"/>
           <div :class="block.type === 'block' ? style.imageBlock : style.chanceBlock">
+            <div v-if="block.class" :class="style[block.class]"/>
             <div v-if="block.type === 'chance'" :class="style.chance"/>
           </div>
           <div v-if="block.price" :class="style.price">{{block.price}} M</div>
@@ -78,6 +80,7 @@
         <div :class="style.block" v-for="(block, index) in rightBlocks" :key="index">
           <div :class="style[block.color]"/>
           <div :class="block.type === 'block' ? style.imageBlock : style.chanceBlock">
+            <div v-if="block.class" :class="style[block.class]"/>
             <div v-if="block.type === 'chance'" :class="style.chance"/>
           </div>
           <div v-if="block.price" :class="style.price">{{block.price}} M</div>
@@ -89,6 +92,7 @@
       <div :class="style.block" v-for="(block, index) in bottomBlocks" :key="index">
         <div :class="style[block.color]"/>
         <div :class="block.type === 'block' ? style.imageBlock : style.chanceBlock">
+          <div v-if="block.class" :class="style[block.class]"/>
           <div v-if="block.type === 'chance'" :class="style.chance"/>
         </div>
         <div v-if="block.price" :class="style.price">
